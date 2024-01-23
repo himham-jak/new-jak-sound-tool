37fa3b6a70a10ab747fb7ff092a282f8f19dac2a
himham-jak <109818991+himham-jak@users.noreply.github.com>
2024-01-23 13:40:17 -0500

    moved server to subfolder

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/ping', (req, res) => {
+  res.send('Pong!');
 });
 
 app.listen(port, () => {

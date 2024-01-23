5be6683fab3b4b3a5be2a7fe9a860412fd2ee556
himham-jak <109818991+himham-jak@users.noreply.github.com>
2024-01-23 15:14:42 -0500

    connect server and client

--- server/server.js
+++ server/server.js
@@ -3,13 +3,17 @@ const app = express();
 const port = 3001;
 
 app.get('/', (req, res) => {
-  res.send('Hello Worlds!');
-});
+  res.send('Hello Worlds!')
+})
 
 app.get('/ping', (req, res) => {
-  res.send('Pong!');
-});
+  res.send('Pong!')
+})
+
+app.get('/api', (req, res) => {
+  res.json({ "users": ["userOne","userTwo","userThree"] })
+})
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+  console.log(`Server running at http://localhost:${port}`)
+})
